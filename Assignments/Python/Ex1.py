d2889d2c78e4ad7e531c61c1f90d57b33f88593d
afaf-wasili <awasly@jazanu.edu.sa>
2024-08-30 12:44:07 +0300

    Your commit message describing the updates

--- Assignments/Python/Ex1.py
+++
@@ -1,8 +0,0 @@
-'''
-Write a program that prompts the user to enter two floats and display their sum on the screen.
-'''
-x  = 3.5
-y = 2.5
-
-a = x + y
-print('sum of two floats=',a)
--- Assignments/Python/Ex2.py
+++
@@ -1,7 +0,0 @@
-'''
-Write a program that asks the user for your name and then welcomes you. The output should look like this:
-
-Enter your name: ...
-Hello ....
-
-'''
