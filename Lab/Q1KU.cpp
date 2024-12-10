de5bc8ec8685e70ba1c0a87481f64364eabce102
Afaf-wasili <awasly@jazanu.edu.sa>
2024-12-10 08:31:17 +0300

    Add files via upload

---
+++ Lab/Q1KU.cpp
@@ -0,0 +1,13 @@
+//Question: Define the variables x and y in separate header file
+void Statements(){
+    if (x < y) {
+        std::cout << x << " is less than " << y << std::endl;
+    } else if (x > y) {
+        std::cout << x << " is greater than " << y << std::endl;
+    } else {
+        std::cout << x << " and " << y << " are equal" << std::endl;
+    }
+}
+
+
+
