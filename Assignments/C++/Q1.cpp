e0f7adc760f85da1fc02f8e6a2133d172f301928
Afaf-wasili <awasly@jazanu.edu.sa>
2026-02-17 08:33:24 +0300

    Update Q1.cpp

--- Assignments/C++/Q1.cpp
+++ Assignments/C++/Q1.cpp
@@ -1,8 +1,8 @@
-'''
+\*
 
 Q1:
 
-Write a Python script that performs the following tasks:
+Write a C++ script that performs the following tasks:
 The marks obtained by a student in 3 different subjects are input by the user. Your
 program should calculate the average of subjects and display the grade. The student gets a
 grade as per the following rules:
@@ -12,4 +12,6 @@ Average Grade
 70-79 C
 60-69 D
 0-59 F
-'''
+
+
+  */
