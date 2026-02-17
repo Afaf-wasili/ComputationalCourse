435511582a628b22dbce5c665735e6ab61b0cb30
Afaf-wasili <awasly@jazanu.edu.sa>
2026-02-17 08:35:40 +0300

    Update Q4.cpp to use C++ for the program

--- Assignments/C++/Q4.cpp
+++ Assignments/C++/Q4.cpp
@@ -1,3 +1,3 @@
-'''
-Q4: Write a Python program to print the numbers from 1 to 10 using a for and while loop.
-'''
+/*
+Q4: Write a C++ program to print the numbers from 1 to 10 using a for and while loop.
+*/
