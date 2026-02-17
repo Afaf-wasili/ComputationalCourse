e39f662fbaf0becc74fc7249031e96eed544b848
Afaf-wasili <awasly@jazanu.edu.sa>
2026-02-17 08:53:10 +0300

    Create Q9.cpp

---
+++ Assignments/C++/Q9.cpp
@@ -0,0 +1,11 @@
+/*
+Write TWO simple C++ programs:
+
+1) FOR loop:
+- Print the numbers from 1 to 10 using a for loop.
+
+2) WHILE loop:
+- Input integers from the user using a while loop.
+- Stop when the user enters 0.
+- Print how many numbers were entered (not counting 0).
+*/
