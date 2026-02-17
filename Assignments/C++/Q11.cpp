48e26dd32261535a79f446d384dd16b64960a01f
Afaf-wasili <awasly@jazanu.edu.sa>
2026-02-17 08:56:44 +0300

    Create Q11.cpp

---
+++ Assignments/C++/Q11.cpp
@@ -0,0 +1,20 @@
+/*
+Write a C++ program that uses a CLASS and a HEADER file:
+
+1) Create a header file named Particle.h that:
+   - defines a class called Particle
+   - contains private data members:
+       name (string)
+       mass (double)
+   - declares public member functions:
+       setData(name, mass)
+       printData()
+
+2) Create a main.cpp file that:
+   - includes "Particle.h"
+   - creates an object of the Particle class
+   - sets the particle name and mass
+   - prints the particle information
+
+Make sure the class definition is written in the header file.
+*/
