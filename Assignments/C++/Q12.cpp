d8fba83c3b8b9852adcdf1c7ffe899de70d6a411
Afaf-wasili <awasly@jazanu.edu.sa>
2026-02-17 08:58:17 +0300

    Add Q12.cpp for relativistic energy calculation
    Implement a C++ program to compute and classify relativistic energy based on momentum and mass inputs.

---
+++ Assignments/C++/Q12.cpp
@@ -0,0 +1,17 @@
+/*
+Write a C++ program that uses if / else if / else:
+
+1) Input two numbers:
+   - momentum p (GeV/c)
+   - mass m (GeV/c^2)
+
+2) Compute the relativistic energy:
+      E = sqrt(p*p + m*m)
+
+3) Classify the energy:
+   - if E > 5 print "High energy particle"
+   - else if E == 5 print "Medium energy particle"
+   - else print "Low energy particle"
+
+4) Print the value of E and the classification.
+*/
