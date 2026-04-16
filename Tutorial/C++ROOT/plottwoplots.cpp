8bdb54e523db3fe006f72eb490f15af8a101f6b8
Afaf-wasili <awasly@jazanu.edu.sa>
2026-04-16 09:33:05 +0300

    Update plottwoplots.cpp

--- Tutorial/C++ROOT/plottwoplots.cpp
+++ Tutorial/C++ROOT/plottwoplots.cpp
@@ -1,37 +1,45 @@
+
+#include "style.h"
+
+//histo.root
+// data, MC
+
 void plottwoplots(){
-
-gROOT-> SetStyle("ATLAS");
-gROOT->ForceStyle();
-
-  // Set font for axis titles and labels                                                     
-
-  TFile* file = TFile::Open("histo.root","READ"); 
-  TH1* data_hist = (TH1*)file->Get("data");
-  TH1* mc_hist = (TH1*)file->Get("MC");
-
-  TCanvas* canvas = new TCanvas("","", 800, 600);
-  //  canvas->SetMargin(0.15, 0.05, 0.15, 0.1);
-  data_hist->SetMarkerColor(kBlue);
-  mc_hist->SetMarkerColor(kRed);
-
-  data_hist->SetStats(0);
-  mc_hist->SetStats(0);
-
-  data_hist->Draw("P");
-  mc_hist->Draw("P SAME");
+  SetGlobalStyle();
 
 
-  data_hist->GetXaxis()->SetTitle("Mass");  
-  data_hist->GetYaxis()->SetTitle("Counts");
-  data_hist->SetTitle("MC and data");
-
-  TLegend* legend = new TLegend(0.5, 0.5, 0.7, 0.7);
-  legend->SetHeader("made by Afaf");
-  legend->AddEntry(data_hist, "Data", "P");
-  legend->AddEntry(mc_hist, "MC", "P");
-  legend->Draw();
+  //open file
+  TFile *file = TFile::Open("histo.root");
 
 
-  canvas->SaveAs("Plots/Data_MC_Histograms.png");
+  //Get histograms
+  TH1 *data = (TH1*)file->Get("data");
+  TH1 *mc = (TH1*)file->Get("MC");
 
-}
+  SetHistogramStyle(data);
+
+  //canvas
+  TCanvas *c = new TCanvas("","",800,600);
+  SetCanvasStyle(c);
+
+  //title x , y
+  data->GetXaxis()->SetTitle("Mass (GeV/c)");
+  data->GetYaxis()->SetTitle("Events");
+
+
+
+  //Draw
+  data->Draw("HIST");
+  mc->Draw("HIST SAME");
+
+
+  //legend
+  TLegend *leg =  CreateLegend();
+  leg->AddEntry(data,"Data-plot","l" ); //l: line
+  leg->AddEntry(mc,"MC-plot","f" ); //f: filling: box
+  leg->Draw();
+
+
+
+  //Save
+  c->SaveAs("plots/Data_MC.png");
