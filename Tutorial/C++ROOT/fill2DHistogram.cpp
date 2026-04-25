4ad8b643a47c5f168c40cf49fa6b0953b7cc5b73
Afaf-wasili <awasly@jazanu.edu.sa>
2026-04-25 23:51:21 +0300

    Refactor fill2DHistogram to improve histogram creation
    Updated the histogram filling logic and added canvas drawing.

--- Tutorial/C++ROOT/fill2DHistogram.cpp
+++ Tutorial/C++ROOT/fill2DHistogram.cpp
@@ -1,16 +1,24 @@
 void fill2DHistogram(){
-
+  //open
   TFile *file = TFile::Open("histo.root");
+  // Get histogrames
   TH1D *data_hist = (TH1D*)file->Get("data");
   TH1D *mc_hist = (TH1D*)file->Get("MC");
-  TH2D *hist_2d = new TH2D("","Data vs MC;Data;MC", 100, 70,120,100,70,120);
+
+  //define 2D
+  TH2D *hist_2D = new TH2D("","", 100,70, 120, 100, 60, 120); //("","",#bin, min_x, max_x, #bin, min_y, max_y)
+    hist_2D->GetXaxis()->SetTitle("Data");
+    hist_2D->GetYaxis()->SetTitle("MC ");
 
 
-  for (int i=1; i <=100; ++i)
-     hist_2d->Fill(data_hist->GetBinCenter(i), mc_hist->GetBinCenter(i));
-  //TCanvas *c = new TCanvas("","", 800,600);
-  hist_2d->Draw("COLZ");
-  //c->Show(); 
-  //c->SaveAs("Plots//Data_MC_2D_Histogram.png"); 
+  //looping: filling
+  for (int i = 1; i<= 120; ++i)
+    hist_2D->Fill(data_hist->GetBinCenter(i), mc_hist->GetBinCenter(i)); //fill(x:y)
+
+
+   TCanvas *c = new TCanvas("","", 800,600);
+
+   hist_2D->Draw("COLZ");
+  c->SaveAs("Data_MC_2D_Histogram.png");
 
 }
